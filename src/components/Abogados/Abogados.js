f003aea90fe7d2524001c8f1cc20e48758e8faaf
HatusP <hpellegr@espol.edu.ec>
2022-01-31 18:56:00 -0500

    Render Completo con nueva estructura de fetch a los endpoints

--- src/components/Abogados/Abogados.js
+++ src/components/Abogados/Abogados.js
@@ -12,75 +12,75 @@ const Abogados = () => {
     let liRetorno = [];
 
     useEffect(() => {
+        console.log(abogados)
+    }, [abogados, liRetorno]);
+
+    useEffect(() => {
         fetchAbogados();
     }, []);
 
-    useEffect(() => {
-        console.log(abogados)
-    }, [abogados]);
+   async function fetchAbogados() {
+       await fetch(endpointAbogado).then(res=>res.json())
+           .then(async abogados => {
+               for (let abog of abogados) {
+                   let usuario = await fetchUsuario(abog.usuario_id)
+                   let telefono = await fetchTelefonos(usuario.telefono_id);
+                   let direccion = await fetchDireccion(usuario.direccion_id);
+                   liRetorno.push({
+                       "nombre":usuario.nombre,
+                       "casos":abog.casos,
+                       "descripcion":abog.descripcion,
+                       "calificacion":abog.calificacion,
+                       "movil":telefono.movil,
+                       "direccion":`${direccion.ciudad}, Sector:${direccion.sector}, ${direccion.callePrincipal}`
+                   })
+               }
+               setAbogados(liRetorno)
+           })
 
-    async function  fetchAbogados(){
-         await fetch(endpointAbogado)
-            .then(res => res.json())
-            .then(data => {
-                let i = 0;
+   }
+    function  fetchUsuario(id){
+        return fetch(`${endpointUsuario}/${id}`).then(res=>res.json());
 
-                for (let aboga of data) {
-                    let casos = data[i].casos
-                    let descripcion = data[i].descripcion
-                    let calif = data[i].calificacion
-                    fetch(endpointUsuario + `/${aboga.usuario_id}`)
-                        .then(res => res.json())
-                        .then(usuario => {
-                            let nombre = usuario.nombre;
-                            fetch(endpointTelefono + `/${usuario.telefono_id}`)
-                                .then(res => res.json())
-                                .then(telefono => {
-                                    let movil = telefono.movil
-                                    fetch(endpointDireccion + `/${usuario.direccion_id}`)
-                                        .then(res => res.json())
-                                        .then(direccion => {
-                                            let dirFinal = direccion.ciudad + "," + direccion.sector + "," + direccion.callePrincipal + "," + direccion.calleSecundaria
-                                            liRetorno.push({
-                                                "casos": casos,
-                                                "descripcion": descripcion,
-                                                "calificacion": calif,
-                                                "nombre": nombre,
-                                                "movil": movil,
-                                                "direccion": dirFinal
-                                            })
-                                        })
-                                })
-                        })
-                    i += 1;
-                }
-                setAbogados(liRetorno);
-            })
     }
+
+    function fetchTelefonos(id){
+        return fetch(`${endpointTelefono}/${id}`).then(res=>res.json());
+
+    }
+
+    function  fetchDireccion(id){
+        return fetch(`${endpointDireccion}/${id}`).then(res=>res.json());
+    }
+    if(abogados.length === 0){
+        return <>Cargando....</>
+    }
+
 
     return (
         <Container id="contenedor">
-            {abogados.map((abog,i)=>(
-                <Row className="fila">
-                    <Card>
-                        <Card.Img variant="top" className="foto"
-                                  src="https://cdn-icons-png.flaticon.com/512/1615/1615060.png"/>
-                        <Card.Body>
-                            <Card.Title>{abog.nombre}</Card.Title>
-                            <Card.Text>
-                                {abog.descripcion}
-                            </Card.Text>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>Numero: {abog.movil}</ListGroupItem>
-                            <ListGroupItem>Direccion: {abog.direccion}</ListGroupItem>
-                            <ListGroupItem>Casos: {abog.casos}</ListGroupItem>
-                        </ListGroup>
-                    </Card>
+            {abogados.map((abog, i) => {
+                    return (
+                        <Row  key={i} className="fila">
+                            <Card>
+                                <Card.Img variant="top" className="foto"
+                                          src="https://cdn-icons-png.flaticon.com/512/1615/1615060.png"/>
+                                <Card.Body>
+                                    <Card.Title>{abog.nombre}</Card.Title>
+                                    <Card.Text>
+                                        {abog.descripcion}
+                                    </Card.Text>
+                                </Card.Body>
+                                <ListGroup className="list-group-flush">
+                                    <ListGroupItem>Numero: {abog.movil}</ListGroupItem>
+                                    <ListGroupItem>Direccion: {abog.direccion}</ListGroupItem>
+                                    <ListGroupItem>Casos: {abog.casos}</ListGroupItem>
+                                </ListGroup>
+                            </Card>
 
-                </Row>
+                        </Row>)
 
-                )
+                }
             )}
         </Container>
     );

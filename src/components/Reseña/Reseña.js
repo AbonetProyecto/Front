d698355b0565ea3acd8a63b84fe6a0f21ed7cf0a
NahinMontoya <73256114+NahinMontoya@users.noreply.github.com>
2022-01-31 22:28:47 -0500

    Se realizaron mejoras en la interfaz grafica

--- src/components/Reseña/Reseña.js
+++ src/components/Reseña/Reseña.js
@@ -3,9 +3,9 @@ import '../../App.css';
 
 
 const Reseña = () =>  {
-  const [newUser,setNewUser] = useState({id: undefined, body:'',username:'', email: ''})
+  const [newUser,setNewUser] = useState({id: undefined, descripcion:''})
   const [users,setUsers] = useState()
-  const endpoint = 'https://jsonplaceholder.typicode.com/comments'
+  const endpoint = 'localhost:3001/api/v1/criticas'
   const fetchUsers= async () =>{
     const response =  await  fetch(endpoint)
     const responseJSON = await response.json()
@@ -14,7 +14,7 @@ const Reseña = () =>  {
   }
 
 const handleNameInput = e =>{
-  setNewUser({...newUser, body: e.target.value})};
+  setNewUser({...newUser, descripcion: e.target.value})};
 
 const handleAddUser = () =>{
   setUsers([...users,newUser])
@@ -26,13 +26,13 @@ useEffect(() => {
 
   return (
     <div className='denver'>
-    <h3 className='letra'>Puede comentar sobre el servicio recibido  </h3>
-    <input className='cuadro'type='text' onChange={handleNameInput} value={newUser.body} placeholder='Ingrese un comentario'/>
-    <button className='bo' onClick={handleAddUser} > Agregar </button>
+    <h3 className='letra'>Puede comentar sobre el servicio recibido </h3>
+    <input className='cuadro'type='text' onChange={handleNameInput} value={newUser.descripcion} placeholder='Ingrese un comentario'/>
+    <button className='bo' onClick={handleAddUser} > Agregar comentario </button>
     <ul>
       {!users ? 'Cargando los comentarios ...' :
       users.map((user,index) => {
-      return<button className='btn2' key ={index}><span className='titulo'>Comentario:</span> {user.body}</button>
+      return<button className='btn2' key ={index}><span className='titulo'>Comentario:</span> {user.descripcion}.  <span className='titulo'>Fecha:</span> 1/02/2022 </button>
       })}
 
     </ul>

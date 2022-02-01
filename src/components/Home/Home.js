1f6e91c4f82ba2e2b1630ce5c2ca80510577c3e2
NahinMontoya <73256114+NahinMontoya@users.noreply.github.com>
2022-02-01 10:39:52 -0500

    se modifico el home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,9 @@ import '../../App.css';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <h1 className='home'>Bienvenidos a ABONET</h1>
-    </>
+      <h2 className='pie'>Nuestra pagina te ayuda a encontrar el abogado ideal para ti </h2>
+      </div>
   );
 }
